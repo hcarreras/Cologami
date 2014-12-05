3eb80fbf76f4594e0f6973a425d3129e6bda6521
hcarreras <haricarreras@gmail.com>
2014-12-05 22:18:23 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harigami::Application.config.secret_key_base = '32a14678d6d5897a5fe738e8f466dab51e4b8d21c952f6d901079141c755cfbcddde406879c0e79e97b24364ff971f01310b34efe6325a0421d4d51cd4b441ac'

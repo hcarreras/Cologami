ec00ece66c5fc62f22d6037d9a387a24d0467945
hcarreras <haricarreras@gmail.com>
2016-03-07 18:23:58 +0100

    New frontpage design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       when "alert" then "alert alert-danger"
     end
   end
+
+  def design_color(design)
+    case design.title
+      when "Natsuki" then "blue-item"
+      when "Ryu" then "red-item"
+      when "Aika" then "yellow-item"
+      when "Mizuki" then "purple-item"
+      when "Miu" then "pink-item"
+    end
+  end
 end

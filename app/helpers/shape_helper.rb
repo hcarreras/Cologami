39e76a4f5593c2d0f836cac97f6723eaf009b117
hcarreras <haricarreras@gmail.com>
2016-07-10 17:22:21 +0200

    Add real pricing and quantities

--- app/helpers/shape_helper.rb
+++ app/helpers/shape_helper.rb
@@ -4,6 +4,10 @@ module ShapeHelper
       ["Pack 5 #{shape.title.pluralize}", 5],
       ["Pack 10 #{shape.title.pluralize}", 10],
       ["Pack 20 #{shape.title.pluralize}", 20],
+      ["Pack 50 #{shape.title.pluralize}", 50],
+      ["Pack 100 #{shape.title.pluralize}", 100],
+      ["Pack 500 #{shape.title.pluralize}", 500],
+      ["Pack 1000 #{shape.title.pluralize}", 1000],
       ["Otras cantidades", "other_quantity"]
     ]
   end
--- app/services/price_calculator.rb
+++ app/services/price_calculator.rb
@@ -6,7 +6,15 @@ class PriceCalculator
       when "10"
         Money.new(500, "EUR")
       when "20"
-        Money.new(800, "EUR")
+        Money.new(900, "EUR")
+      when "50"
+        Money.new(2000, "EUR")
+      when "100"
+        Money.new(3500, "EUR")
+      when "500"
+        Money.new(15000, "EUR")
+      when "1000"
+        Money.new(25000, "EUR")
       when "other_quantity"
         "other_quantity"
     end
